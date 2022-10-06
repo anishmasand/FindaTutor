392ea66daca2b7bc2671dad25d89c7935164debc
Anish Masand <anish.b.masand@gmail.com>
2022-10-06 20:15:19 +0530

    solved version errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, componse} from 'redux';
+import thunk from "redux-thunk";
 import App from './App';
 //import './index.css';
 
