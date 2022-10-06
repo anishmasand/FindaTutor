981da218d440270d15514cd6b3d6e6dfc8c4aa3a
Anish Masand <anish.b.masand@gmail.com>
2022-10-06 20:26:54 +0530

    redux1

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, componse} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from "redux-thunk";
+import reducers from "./reducers";
 import App from './App';
 //import './index.css';
+
+
+const store = createStore(reducers,compose(applyMiddleware(thunk)))
 
 ReactDOM.render(<App/>, document.getElementById('root')); //we render this inside the "root" id which is the 'id' of our main div in the main html page.
